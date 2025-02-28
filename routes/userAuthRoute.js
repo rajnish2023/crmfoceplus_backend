ee92defd37c9384be3ae4a1fe691b8bdc9a532e9
rajnish2023 <rajnish.kumar@dynamicssquare.com>
2025-02-28 09:47:43 +0530

    code updated userauth

--- routes/userAuthRoute.js
+++ routes/userAuthRoute.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/var/data/uploads/profilePics');
+      cb(null, '/var/data/uploads');
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + file.originalname);  
